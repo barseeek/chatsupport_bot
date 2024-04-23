9e4df915b4ce1d178b2969de88aabc626a40b94b
Nikita <barskii.no@outlook.com>
2024-04-23 10:21:40 +0300

    Fix global variables

--- tg_bot.py
+++ tg_bot.py
@@ -1,11 +1,11 @@
 import logging
 
 import environs
+
 from dialogflow import create_api_key, detect_intent_texts, read_credentials
 from log import TelegramLogsHandler
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-
 
 logger = logging.getLogger('bot')
 
@@ -15,15 +15,31 @@ def start(update: Update, context: CallbackContext) -> None:
 
 
 def reply_to_user(update: Update, context: CallbackContext) -> None:
+    project_id = context.bot_data['PROJECT_ID']
+    language_code = context.bot_data['LANGUAGE_CODE']
     logger.info('Получено сообщение в tg "{}" от {}'.format(update.message.text, update.effective_chat.id))
-    message_text = detect_intent_texts(PROJECT_ID, CHAT_ID, update.message.text, LANGUAGE_CODE).fulfillment_text
+    message_text = detect_intent_texts(project_id, update.message.chat.id, update.message.text, language_code).fulfillment_text
     update.message.reply_text(message_text)
 
 
 def main():
-    create_api_key(PROJECT_ID, CHAT_ID)
+    env = environs.Env()
+    env.read_env()
+
+    language_code = env.str('LANGUAGE_CODE')
+    project_id = read_credentials(env.str('GOOGLE_APPLICATION_CREDENTIALS'))['quota_project_id']
+    chat_id = env.str('TELEGRAM_CHAT_ID')
+
+    telegram_log_token = env.str('TELEGRAM_LOG_BOT_TOKEN')
+    tg_handler = TelegramLogsHandler(chat_id, telegram_log_token)
+    logger.addHandler(tg_handler)
+    logger.setLevel(env.str('LOG_LEVEL', 'INFO'))
+
+    create_api_key(project_id, chat_id)
     updater = Updater(env.str('TELEGRAM_BOT_TOKEN'))
     dispatcher = updater.dispatcher
+    dispatcher.bot_data['PROJECT_ID'] = project_id
+    dispatcher.bot_data['LANGUAGE_CODE'] = language_code
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply_to_user))
     updater.start_polling()
@@ -32,15 +48,4 @@ def main():
 
 
 if __name__ == '__main__':
-    env = environs.Env()
-    env.read_env()
-
-    LANGUAGE_CODE = env.str('LANGUAGE_CODE')
-    PROJECT_ID = read_credentials(env.str('GOOGLE_APPLICATION_CREDENTIALS'))['quota_project_id']
-    CHAT_ID = env.str('TELEGRAM_CHAT_ID')
-    telegram_log_token = env.str('TELEGRAM_LOG_BOT_TOKEN')
-    tg_handler = TelegramLogsHandler(CHAT_ID, telegram_log_token)
-    logger.addHandler(tg_handler)
-    logger.setLevel(env.str('LOG_LEVEL', 'INFO'))
-
     main()
